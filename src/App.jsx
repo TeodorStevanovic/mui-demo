9e0a970518f723c374f43bc43b2c51c14bed2094
TeodorStevanovic <stevanovicteodor002@gmail.com>
2026-01-11 14:25:12 +0100

    added Controller from react-hook-form to manage MUI textarea input

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,17 @@ import {
   ListItem,
   ListItemText,
 } from "@mui/material";
+import { useForm, Controller } from "react-hook-form";
 
 const App = () => {
   const [dialog, setDialog] = useState(false);
-  const [newTask, setNewTask] = useState("");
   const [tasks, setTasks] = useState([]);
+
+  const { control, handleSubmit, reset } = useForm({
+    defaultValues: {
+      task: "",
+    },
+  });
 
   const handleOpen = () => {
     setDialog(true);
@@ -27,9 +33,9 @@ const App = () => {
     setDialog(false);
   };
 
-  const handleAddTask = () => {
-    setTasks((prevTask) => [...prevTask, newTask]);
-    setNewTask("");
+  const handleAddTask = (data) => {
+    setTasks((prevTask) => [...prevTask, data.task]);
+    reset();
     handleClose();
   };
 
@@ -56,16 +62,21 @@ const App = () => {
       <Dialog open={dialog} onClose={handleClose}>
         <DialogTitle>Add New Task</DialogTitle>
         <DialogContent>
-          <TextareaAutosize
-            maxRows={20}
-            value={newTask}
-            style={{ width: "100%" }}
-            onChange={(e) => setNewTask(e.target.value)}
+          <Controller
+            name="task"
+            control={control}
+            render={({ field }) => (
+              <TextareaAutosize
+                {...field}
+                maxRows={4}
+                style={{ width: "100%" }}
+              />
+            )}
           />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancel</Button>
-          <Button onClick={handleAddTask}>Add</Button>
+          <Button onClick={handleSubmit(handleAddTask)}>Add</Button>
         </DialogActions>
       </Dialog>
     </Box>
