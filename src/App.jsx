0bd513baf198b180a81c026237820b6fba5c2040
TeodorStevanovic <stevanovicteodor002@gmail.com>
2026-01-13 10:30:21 +0100

    added user search filter

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import {
   Dialog,
   DialogTitle,
   DialogContent,
+  TextField,
 } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import PersonIcon from "@mui/icons-material/Person";
@@ -21,6 +22,7 @@ const App = () => {
   const [users, setUsers] = useState([]);
   const [selectUser, setSelectUser] = useState(null);
   const [openDialog, setOpenDialog] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleOpen = (clickedUser) => {
     setSelectUser(clickedUser);
@@ -31,7 +33,11 @@ const App = () => {
     setOpenDialog(false);
   };
 
-  console.log(users);
+  const filteredUsers = users.filter(
+    (user) =>
+      user.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      user.username.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -48,48 +54,66 @@ const App = () => {
           <IconButton edge="start" aria-label="menu" sx={{ mr: 2 }}>
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div">
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Users Profile
           </Typography>
+          <Box>
+            <TextField
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              variant="outlined"
+              size="small"
+              placeholder="Search"
+              sx={{ backgroundColor: "white", borderRadius: 1 }}
+            />
+          </Box>
         </Toolbar>
       </AppBar>
       <Box sx={{ padding: 1 }}>
         <Grid container spacing={10}>
-          {users.map((user) => (
-            <Grid item xs={12} sm={6} md={4} key={user.id}>
-              <Card
-                sx={{
-                  width: "100%",
-                  display: "flex",
-                  alignItems: "center",
-                  flexDirection: "column",
-                  backgroundColor: "#1976d2",
-                  borderRadius: 2,
-                }}
-              >
-                <PersonIcon sx={{ width: 50, height: 50 }} />
-                <CardHeader
-                  title={user.name}
-                  subheader={user.username}
-                  sx={{ width: 350, backgroundColor: "#fff" }}
-                />
-                <Button
-                  variant="text"
-                  onClick={() => handleOpen(user)}
-                  sx={{ color: "#fff" }}
+          {filteredUsers.length === 0 ? (
+            <Typography>User Not Found</Typography>
+          ) : (
+            filteredUsers.map((user) => (
+              <Grid item xs={12} sm={6} md={4} key={user.id}>
+                <Card
+                  sx={{
+                    width: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                    flexDirection: "column",
+                    backgroundColor: "#1976d2",
+                    borderRadius: 2,
+                  }}
                 >
-                  Learn more
-                </Button>
-              </Card>
-            </Grid>
-          ))}
+                  <PersonIcon sx={{ width: 50, height: 50 }} />
+                  <CardHeader
+                    title={user.name}
+                    subheader={user.username}
+                    sx={{ width: 350, backgroundColor: "#fff" }}
+                  />
+                  <Button
+                    variant="text"
+                    onClick={() => handleOpen(user)}
+                    sx={{ color: "#fff" }}
+                  >
+                    Learn more
+                  </Button>
+                </Card>
+              </Grid>
+            ))
+          )}
         </Grid>
         <Dialog open={openDialog} onClose={handleClose}>
           <DialogTitle>About user:</DialogTitle>
-          <DialogContent sx={{ display: "flex", flexDirection: "column", gap: "10px"}}>
+          <DialogContent
+            sx={{ display: "flex", flexDirection: "column", gap: "10px" }}
+          >
             <Typography> E-mail: {selectUser?.email} </Typography>
             <Typography>Phone Number: {selectUser?.phone} </Typography>
-            <Typography>Address: {selectUser?.address.street} / {selectUser?.address.city}</Typography>
+            <Typography>
+              Address: {selectUser?.address.street} / {selectUser?.address.city}
+            </Typography>
             <Typography>Website: {selectUser?.website}</Typography>
             <Typography>Company: {selectUser?.company.name}</Typography>
           </DialogContent>
