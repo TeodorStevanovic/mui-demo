a8fca34df123fb33856b4399000f3caa5b74e3b0
TeodorStevanovic <stevanovicteodor002@gmail.com>
2026-01-16 18:13:46 +0100

    add basic home landing content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { Box, Typography, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
   return (
-    <>
-      <p>Home Page</p>
-    </>
+    <Box>
+      <Typography variant="h3">Users Profile</Typography>
+      <Typography variant="h5">
+        Browse and explore user profiles with detailed information.
+      </Typography>
+      <Typography variant="p">
+        This application allows you to view user profiles fetched from an
+        external API. You can search users, open detailed profile information,
+        and navigate through the app using a modern React setup.
+      </Typography>
+      <Button
+        variant="contained"
+        onClick={() => {
+          navigate("/users");
+        }}
+      >
+        View Users
+      </Button>
+    </Box>
   );
 };
 
