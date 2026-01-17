a96f930b804e6e945e086de9d53b83f41147c4f1
TeodorStevanovic <stevanovicteodor002@gmail.com>
2026-01-17 18:51:17 +0100

    style: polish home page layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,24 +5,41 @@ import { useNavigate } from "react-router-dom";
 const Home = () => {
   const navigate = useNavigate();
   return (
-    <Box>
-      <Typography variant="h3">Users Profile</Typography>
-      <Typography variant="h5">
-        Browse and explore user profiles with detailed information.
-      </Typography>
-      <Typography variant="p">
-        This application allows you to view user profiles fetched from an
-        external API. You can search users, open detailed profile information,
-        and navigate through the app using a modern React setup.
-      </Typography>
-      <Button
-        variant="contained"
-        onClick={() => {
-          navigate("/users");
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          flexDirection: "column",
+          gap: "25px",
         }}
       >
-        View Users
-      </Button>
+        <Typography variant="h3">Users Profile</Typography>
+        <Typography variant="h5">
+          Browse and explore user profiles with detailed information.
+        </Typography>
+        <Typography variant="p">
+          This application allows you to view user profiles fetched from an
+          external API. You can search users, open detailed profile information,
+          and navigate through the app using a modern React setup.
+        </Typography>
+        <Button
+          variant="contained"
+          onClick={() => {
+            navigate("/users");
+          }}
+        >
+          View Users
+        </Button>
+      </Box>
     </Box>
   );
 };
